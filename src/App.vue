0a7660ad259348a0a404f7301f4c87e672c88118
MindulaHewadewage <mindufernando@gmail.com>
2023-03-15 19:46:17 +0100

    imported axios+configuration

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <script>
+import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
+const apiBaseUrl = 'http://127.0.0.1:8000/api/'
 export default {
   name: 'App',
-  components: { AppHeader }
+  components: { AppHeader },
+  data: () => ({
+    projects: []
+  }),
+  methods: {
+    fetchProjects() {
+      axios.get(apiBaseUrl + 'projects').then(res => {
+        this.projects = res.data;
+      });
+    }
+  },
+  created() {
+    this.fetchProjects();
+  }
 };
 </script>
 
@@ -10,6 +25,7 @@ export default {
 
 <template>
   <AppHeader />
+  <main class="container"></main>
 </template>
 
 <style></style>
