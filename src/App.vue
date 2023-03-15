e21838cdaf832afbd16b3454342da44ca9edc1d8
MindulaHewadewage <mindufernando@gmail.com>
2023-03-15 21:02:45 +0100

    card

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script>
 import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
+import AppHeader from './components/projects/ProjectCard.vue';
 const apiBaseUrl = 'http://127.0.0.1:8000/api/'
 export default {
   name: 'App',
-  components: { AppHeader },
+  components: { AppHeader, ProjectCard },
   data: () => ({
     projects: []
   }),
@@ -26,10 +27,8 @@ export default {
 <template>
   <AppHeader />
   <main class="container">
-    <ul v-if="projects.length">
-      <li v-for="project in projects" :key="project.id">{{ project.title }}</li>
-    </ul>
-    <h5 v-else>Non ci sono post</h5>
+    <ProjectCard />
+
   </main>
 </template>
 
---
+++ src/components/projects/ProjectCard.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props: { projects: object }
+}
+</script>
+
+
+<!-- <template>
+    <section id="project-card">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">{{ project.title }}</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                    content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </section>
+</template>  -->
+
+
+<style></style>
